f9ae71c3e7b2d6123eb82392dfdd2c2d269a525c
zuijiuzhou <yong-zh@qq.com>
2025-03-17 22:45:36 +0800

    --

--- src/glr/engine/ClipControl.h
+++ src/glr/engine/ClipControl.h
@@ -7,11 +7,31 @@ class GLR_API ClipControl : public StateAttribute {
     VI_OBJECT_META;
 
   public:
+    enum Origin
+    {
+        LOWER_LEFT = HGL_LOWER_LEFT,
+        UPPER_LEFT = HGL_UPPER_LEFT
+    };
+
+    enum Depth
+    {
+        NEGATIVE_ONE_TO_ONE = HGL_NEGATIVE_ONE_TO_ONE,
+        ZERO_TO_ONE         = HGL_ZERO_TO_ONE
+    };
+
+  public:
     ClipControl();
+    ClipControl(Origin origin, Depth depth);
     virtual ~ClipControl();
 
   public:
     virtual Type getType() const override;
+
+    void   setOrigin(Origin origin);
+    Origin getOrigin() const;
+
+    void  setDepth(Depth origin);
+    Depth getDepth() const;
 
   protected:
     virtual void apply(State& state) const override;
--- src/glr/engine/ColorMask.h
+++ src/glr/engine/ColorMask.h
@@ -8,10 +8,47 @@ class GLR_API ColorMask : public StateAttribute {
 
   public:
     ColorMask();
+    ColorMask(bool red, bool green, bool blue, bool alpha);
     virtual ~ColorMask();
 
   public:
     virtual Type getType() const override;
+
+    void setMask(bool red, bool green, bool blue, bool alpha);
+    void getMask(bool& red, bool& green, bool& blue, bool& alpha) const;
+
+    void setRed(bool mask);
+    bool getRed() const;
+
+    void setGreen(bool mask);
+    bool getGreen() const;
+
+    void setBlue(bool mask);
+    bool getBlue() const;
+
+    void setAlpha(bool mask);
+    bool getAlpha() const;
+
+  protected:
+    virtual void apply(State& state) const override;
+
+  private:
+    VI_OBJECT_DATA;
+};
+
+class GLR_API ColorMaski : public ColorMask {
+    VI_OBJECT_META;
+
+  public:
+    ColorMaski();
+    ColorMaski(GLuint_t buf, bool red, bool green, bool blue, bool alpha);
+    virtual ~ColorMaski();
+
+  public:
+    virtual Type getType() const override;
+
+    void     setIndex(GLuint_t);
+    GLuint_t getIndex() const;
 
   protected:
     virtual void apply(State& state) const override;
