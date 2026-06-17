0c42fbac4effd274747f922fc9489b839a5f4a2a
zhouyong <zhouyong@vision3d.cn>
2026-06-17 16:55:45 +0800

    --

--- src/xg/xviewer/src/io/BrepLoader.cpp
+++ src/xg/xviewer/src/io/BrepLoader.cpp
@@ -21,6 +21,7 @@
 #include <TopExp.hxx>
 #include <TopExp_Explorer.hxx>
 #include <TopTools.hxx>
+#include <TopTools_IndexedMapOfShape.hxx>
 #include <TopoDS.hxx>
 #include <TopoDS_Compound.hxx>
 #include <TopoDS_Edge.hxx>
