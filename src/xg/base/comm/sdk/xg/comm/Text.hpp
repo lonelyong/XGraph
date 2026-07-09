5af64356b826c928b32c2e9a9f4f7dcd0593e5c4
zhouyong <zhouyong@vision3d.cn>
2026-07-09 16:56:26 +0800

    --

--- src/xg/base/comm/sdk/xg/comm/Text.hpp
+++ src/xg/base/comm/sdk/xg/comm/Text.hpp
@@ -1,20 +1,15 @@
 ﻿#pragma once
 #include <string>
 
-namespace xg {
-std::wstring gbkToUnicode(const std::string& str);
+namespace xg
+{
 
-std::string unicodeToGbk(const std::wstring& str);
+std::wstring utf8ToWide(const std::string& str);
 
-std::wstring utf8ToUnicode(const std::string& str);
-
-std::string unicodeToUtf8(const std::wstring& str);
-
-std::string gbkToUtf8(const std::string& str);
-
-std::string utf8ToGbk(const std::string& str);
+std::string wideToUtf8(const std::wstring& str);
 
 std::string local8bitToUtf8(const std::string& str);
 
 std::string utf8ToLocal8bit(const std::string& str);
+
 } // namespace xg
--- src/xg/base/comm/src/comm/Text.cpp
+++ src/xg/base/comm/src/comm/Text.cpp
@@ -9,91 +9,59 @@
 #endif // _WIN32
 
 
-namespace xg {
-namespace {
+namespace xg
+{
+namespace
+{
 
-#ifdef _WIN32
-class codecvt_gbk : public std::codecvt_byname<wchar_t, char, std::mbstate_t> {
-  public:
-    codecvt_gbk()
-      : codecvt_byname("zh_CN")
-    {
-    }
-};
+// #ifdef _WIN32
+// class codecvt_gbk : public std::codecvt_byname<wchar_t, char, std::mbstate_t> {
+//   public:
+//     codecvt_gbk()
+//       : codecvt_byname("zh_CN")
+//     {}
+// };
+//
+// std::wstring_convert<codecvt_gbk> s_GbkConvert;
+// #endif
 
-std::wstring_convert<codecvt_gbk> s_GbkConvert;
-#endif
-
-std::wstring_convert<std::codecvt_utf8<wchar_t>> s_Utf8Convert;
+std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> s_utf8_wchar_cvt;
 
 } // namespace
 
-#ifdef _WIN32
-std::wstring gbkToUnicode(const std::string& str) {
-    return s_GbkConvert.from_bytes(str);
-}
-std::string unicodeToGbk(const std::wstring& str) {
-    return s_GbkConvert.to_bytes(str);
-}
-#else
-std::wstring gbkToUnicode(const std::string& str) {
-    return utf8ToUnicode(str);
-}
-std::string unicodeToGbk(const std::wstring& str) {
-    return unicodeToUtf8(str);
-}
-#endif
+std::wstring utf8ToWide(const std::string& str)
+{ return s_utf8_wchar_cvt.from_bytes(str); }
 
-std::wstring utf8ToUnicode(const std::string& str) {
-    return s_Utf8Convert.from_bytes(str);
-}
-std::string unicodeToUtf8(const std::wstring& str) {
-    return s_Utf8Convert.to_bytes(str);
+std::string wideToUtf8(const std::wstring& str)
+{ return s_utf8_wchar_cvt.to_bytes(str); }
+
+std::string local8bitToUtf8(const std::string& str)
+{
+    if (str.empty()) { return str; }
+#ifdef _WIN32
+    // cbMultiByte == -1时，MultiByteToWideChar返回的长度包含'\0'，比实际字符数多1
+    const int    tmp_len = MultiByteToWideChar(CP_ACP, 0, str.data(), -1, nullptr, 0);
+    std::wstring tmp_str(tmp_len - 1, wchar_t{});
+    MultiByteToWideChar(CP_ACP, 0, str.data(), -1, tmp_str.data(), tmp_len);
+    return wideToUtf8(tmp_str);
+#else
+    return str;
+#endif
 }
 
+std::string utf8ToLocal8bit(const std::string& str)
+{
+    if (str.empty()) { return str; }
 #ifdef _WIN32
-std::string gbkToUtf8(const std::string& str) {
-    return s_Utf8Convert.to_bytes(s_GbkConvert.from_bytes(str));
-}
-std::string utf8ToGbk(const std::string& str) {
-    std::u8string s((const char8_t*)str.data());
-    return s_GbkConvert.to_bytes(s_Utf8Convert.from_bytes(str));
-}
+    auto wstr           = utf8ToWide(str);
+    // cbMultiByte == -1时，WideCharToMultiByte返回的长度包含'\0'，比实际字符数多1
+    int         tmp_len = WideCharToMultiByte(CP_ACP, 0, wstr.data(), -1, nullptr, 0, nullptr, nullptr);
+    std::string tmp_str(tmp_len - 1, char{});
+    WideCharToMultiByte(CP_ACP, 0, wstr.data(), -1, tmp_str.data(), tmp_len, nullptr, nullptr);
+    return tmp_str;
 #else
-std::string gbkToUtf8(const std::string& str) {
     return str;
-}
-std::string utf8ToGbk(const std::string& str) {
-    return str;
-}
 #endif
+}
 
-std::string local8bitToUtf8(const std::string& str) {
-    if (str.empty()) {
-        return str;
-    }
-#ifdef _WIN32
-    int          tmpLen = MultiByteToWideChar(CP_ACP, 0, str.data(), -1, NULL, 0);
-    std::wstring tempStr(tmpLen + 1, WCHAR(0));
-    MultiByteToWideChar(CP_ACP, 0, str.data(), -1, tempStr.data(), tmpLen + 1);
-    return unicodeToUtf8(tempStr);
-#else
-    return str;
-#endif
-}
-std::string utf8ToLocal8bit(const std::string& str) {
-    if (str.empty()) {
-        return str;
-    }
-#ifdef _WIN32
-    /* auto         wstr   = utf8ToUnicode(str);
-     int          tmpLen = WideCharToMultiByte(CP_ACP, 0, wstr.data(), -1, NULL, 0);
-     std::wstring tempStr(tmpLen + 1, WCHAR(0));
-     MultiByteToWideChar(CP_ACP, 0, str.data(), -1, tempStr.data(), tmpLen + 1);
-     return unicodeToUtf8(tempStr);*/
-    return str;
-#else
-    return str;
-#endif
-}
 } // namespace xg
